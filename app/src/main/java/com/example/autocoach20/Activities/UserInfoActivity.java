f9904c750fa5cbf5d4514b5ca807abb3ffa44b70
xueningleng <sherryleng27@gmail.com>
2020-11-12 19:53:14 -0800

    user info ui

--- app/src/main/java/com/example/autocoach20/Activities/UserInfoActivity.java
+++ app/src/main/java/com/example/autocoach20/Activities/UserInfoActivity.java
@@ -1,14 +1,15 @@
 package com.example.autocoach20.Activities;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StartAutoCoachActivity extends AppCompatActivity {
+public class UserInfoActivity extends AppCompatActivity {
+    EditText userAge;
+    EditText userGender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
-
-
 }
