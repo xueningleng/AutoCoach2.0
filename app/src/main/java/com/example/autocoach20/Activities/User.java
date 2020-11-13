cd831f94ceaaee384825988636cdba549e2edc18
xueningleng <sherryleng27@gmail.com>
2020-11-12 18:49:00 -0800

    User bio info

--- app/src/main/java/com/example/autocoach20/Activities/User.java
+++ app/src/main/java/com/example/autocoach20/Activities/User.java
@@ -17,15 +17,42 @@ public class User {
     private double personalSwervesScore = 87;
     private double driverPersonalScore = 86;
 
+    //biological information
+    private int age;
+    private int gender; // 0 for female, 1 for male
+    //normal default sets to average for humans, could manually set to personalized
+    private double normal_blood_pressure_systolic = 120; //mm Hg, maximum
+    private double normal_blood_pressure_diastolic = 80; //mm Hg, minimum
+
+    private double normal_heart_rate_max;
+
+    public double current_heart_rate;
+    public double current_blood_pressure;
     double[] driverMeans = new double[]{
             //0.06031552, 0.05229149, 0.019399881, 0.017509023, 0.015382588, 0.8554055, 0.08283522, 0.06175925, 0.8509335, 0.09026337, 0.05880312, 0.8505389, 0.08672229, 0.06273885, 0.8562540, 0.08586243, 0.05788359
             0.02031552, 0.0429149, 0.02399881, 0.07009023, 0.018382588, 0.9554055, 0.07283522, 0.05175925, 0.9509335, 0.05626337, 0.04680312, 0.9005389, 0.05272229, 0.07073885, 0.5062540, 0.08386243, 0.06788359
     };
 
-    public User(FirebaseUser u){
+    public User(FirebaseUser u, int userAge, int userGender){
         user_id = u.getUid();
         user_email = u.getEmail();
         user_name = u.getDisplayName();
+        this.age = userAge;
+        this.gender = userGender;
     };
+
+    private void SetNormalHeartRate(int heartRateMax){
+        if (heartRateMax >=0) { // no input
+            this.normal_heart_rate_max = heartRateMax;
+        }
+        else{
+            switch(gender){
+                case 0: //female
+                    this.normal_heart_rate_max = 84;
+                case 1: //male
+                    this.normal_heart_rate_max = 82;
+            }
+        }
+    }
 
 }
