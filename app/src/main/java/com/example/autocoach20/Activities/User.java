7d311ceb92d2203d33557d35403089bcf0e20de0
xueningleng <sherryleng27@gmail.com>
2020-11-12 19:04:54 -0800

    User bio info update

--- app/src/main/java/com/example/autocoach20/Activities/User.java
+++ app/src/main/java/com/example/autocoach20/Activities/User.java
@@ -27,7 +27,8 @@ public class User {
     private double normal_heart_rate_max;
 
     public double current_heart_rate;
-    public double current_blood_pressure;
+    public double current_blood_pressure_systolic;
+    public double current_blood_pressure_diastolic;
     double[] driverMeans = new double[]{
             //0.06031552, 0.05229149, 0.019399881, 0.017509023, 0.015382588, 0.8554055, 0.08283522, 0.06175925, 0.8509335, 0.09026337, 0.05880312, 0.8505389, 0.08672229, 0.06273885, 0.8562540, 0.08586243, 0.05788359
             0.02031552, 0.0429149, 0.02399881, 0.07009023, 0.018382588, 0.9554055, 0.07283522, 0.05175925, 0.9509335, 0.05626337, 0.04680312, 0.9005389, 0.05272229, 0.07073885, 0.5062540, 0.08386243, 0.06788359
@@ -55,4 +56,31 @@ public class User {
         }
     }
 
+    public void setCurrentBloodPressure(int bpmax, int bpmin){
+        this.current_blood_pressure_systolic = bpmax;
+        this.current_blood_pressure_diastolic = bpmin;
+    }
+
+    public void setCurrentHeartRate(int hr){
+        this.current_heart_rate = hr;
+    }
+    public String checkBloodPressure(){
+        if (current_blood_pressure_systolic>normal_blood_pressure_systolic){
+            return ("WARNING: HIGH BLOOD PRESSURE");
+        }
+        else if (current_blood_pressure_diastolic<normal_blood_pressure_diastolic){
+            return ("WARNING: LOW BLOOD PRESSURE");
+        }
+        else{
+            return ("NORMAL BLOOD PRESSURE");
+        }
+    }
+    public String checkHeartRate(){
+        if (current_heart_rate > normal_heart_rate_max) {
+            return ("WARNING: DANGEROUS HEART RATE");
+        }
+        else{
+            return ("NORMAL HEART RATE");
+        }
+    }
 }
