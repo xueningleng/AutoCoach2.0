c3160a366df0d40887faee7096dbe16f9361ddd6
xueningleng <sherryleng27@gmail.com>
2021-01-26 20:39:18 -0800

    database using mysql

--- app/src/main/java/com/example/autocoach20/Activities/DBOperations.java
+++ app/src/main/java/com/example/autocoach20/Activities/DBOperations.java
@@ -7,6 +7,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
+import java.sql.*;
+
+
 public class DBOperations {
     Statement stmt = null;
     ResultSet rs = null;
@@ -15,36 +19,74 @@ public class DBOperations {
     public DBOperations(){
 
     }
-    public static void main(String args[]){
+   /* public static void main(String args[])throws Exception{
         DBOperations db = new DBOperations();
-        db.connect();
-        db.writeSpeed("test",123,1,60);
-        db.fetch("speed","speed");
+        db.createTable("tripRecord");
+        //db.getAutoKey("tripRecord");
+        db.writeSpeed("tripRecord" ,"user1", 124,1,62);
+        db.fetch("tripRecord","speed");
     }
+*/
 
-    public void connect() {
+
+    public void createTable(String tableName)throws SQLException{
         try {
             String myDriver = "org.gjt.mm.mysql.Driver";
             String myUrl = "jdbc:mysql://localhost:3306/autocoach";
             Class.forName(myDriver);
             Connection conn = DriverManager.getConnection(myUrl, "root", "password");
+            //System.out.println("Connected to database ");
+            Statement stmt = conn.createStatement();
+            String query = "CREATE TABLE "+tableName+"(" +
+                    "ID INT NOT NULL, " +
+                    "userName VARCHAR (20) NOT NULL, " +
+                    "userId INT NOT NULL, " +
+                    "tripId INT NOT NULL, " +
+                    "speed DECIMAL, " +
+                    "PRIMARY KEY (ID));";
+            stmt.execute(query);
+            System.out.println("Table Created......");
         }
         catch(Exception e){
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("Cause: " + e.getCause());
         }
-    }
+        finally{
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
 
-    public void fetch(String table, String field){
-        try {
-            if (stmt.execute("SELECT "+field+" FROM "+ table)) {
-                rs = stmt.getResultSet();
+                stmt = null;
             }
         }
-        catch(SQLException e){
+
+    }
+    public void fetch(String table, String field) throws Exception{
+        try {
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "jdbc:mysql://localhost:3306/autocoach";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "password");
+            //System.out.println("Connected to database ");
+            Statement stmt = conn.createStatement();
+            if (stmt.execute("SELECT "+field+" FROM "+ table+";")) {
+                rs = stmt.getResultSet();
+            }
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+        }
+        catch(Exception e){
             System.out.println("SQLException: " + e.getMessage());
-            System.out.println("SQLState: " + e.getSQLState());
-            System.out.println("VendorError: " + e.getErrorCode());
+            System.out.println("Cause: " + e.getCause());
         }
         finally{
             if (rs != null) {
@@ -54,7 +96,6 @@ public class DBOperations {
 
                 rs = null;
             }
-
             if (stmt != null) {
                 try {
                     stmt.close();
@@ -65,15 +106,61 @@ public class DBOperations {
         }
     }
 
-    public void writeSpeed(String userName, int userId, int tripId, int speed){
+    public int getAutoKey(String table)throws SQLException{
+        int autoIncKeyFromFunc = 0;
         try {
-            int autoIncKeyFromFunc = -1;
-            rs = stmt.executeQuery("SELECT LAST_INSERT_ID()");
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "jdbc:mysql://localhost:3306/autocoach";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "password");
+            //System.out.println("Connected to database ");
+            Statement stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT * from " + table +";");
 
-            while(rs.next()) {
+            while (rs.next()) {
                 autoIncKeyFromFunc = rs.getInt(1);
             }
-            String query =" insert into speed(id, userName, userId, tripId, speed)"+" values (?, ?, ?, ?, ?)";
+            //System.out.println("next key " + autoIncKeyFromFunc);
+        }
+        catch(Exception e){
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("Cause: " + e.getCause());
+        }
+        finally{
+            if (autoIncKeyFromFunc<=0){
+                autoIncKeyFromFunc = 1;
+            }
+            else{
+                autoIncKeyFromFunc++;
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { } // ignore
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+                stmt = null;
+            }
+        }
+
+        //System.out.println("key is "+autoIncKeyFromFunc);
+        return autoIncKeyFromFunc;
+    }
+    public void writeSpeed(String table, String userName, int userId, int tripId, int speed)throws Exception{
+
+        int autoIncKeyFromFunc = getAutoKey(table);
+        try{
+            String myDriver = "org.gjt.mm.mysql.Driver";
+            String myUrl = "jdbc:mysql://localhost:3306/autocoach";
+            Class.forName(myDriver);
+            Connection conn = DriverManager.getConnection(myUrl, "root", "password");
+            System.out.println("Connected to database ");
+            Statement stmt = conn.createStatement();
+            String query =" insert into "+table+"(id, userName, userId, tripId, speed)"+" values (?, ?, ?, ?, ?);";
             PreparedStatement preparedStmt = conn.prepareStatement(query);
             preparedStmt.setInt    (1, autoIncKeyFromFunc);
             preparedStmt.setString (2, userName);
@@ -83,11 +170,23 @@ public class DBOperations {
             preparedStmt.execute();
             conn.close();
         }
-        catch (Exception e)
-        {
-            System.err.println("Got an exception!");
-            System.err.println(e.getMessage());
+        catch(Exception e){
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("Cause: " + e.getCause());
+        }
+        finally{
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { } // ignore
+                rs = null;
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+                stmt = null;
+            }
         }
     }
-
 }
